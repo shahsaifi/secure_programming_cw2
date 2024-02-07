09e558b69ac77d1265d9a694f75639fd6f3d4ac2
Mohd Sahnawaz <sahnawaz@voldemort.local>
2024-02-07 19:42:19 +0800

    skel

---
+++ init_db.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+connection = sqlite3.connect('database.db')
+
+
+with open('schema.sql') as f:
+    connection.executescript(f.read())
+
+cur = connection.cursor()
+
+cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+            ('Test Post', 'Content for the first post')
+            )
+
+connection.commit()
+connection.close()
